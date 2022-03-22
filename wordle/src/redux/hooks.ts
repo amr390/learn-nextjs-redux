7a60fdc8133fd067c0a4373ac2bca14cc0a01c7a
amr <anma@manning.com>
2022-03-22 07:47:38 +0100

    using tailwindcss to quick style the app

--- wordle/src/redux/hooks.ts
+++ wordle/src/redux/hooks.ts
@@ -1,5 +1,5 @@
-import { TypeUseSelectorHook, useDispatch, useSelector} from 'react-redux'
+import { TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux'
 import type { RootState, AppDispatch } from '@redux/store'
 
 export const useAppDispatch = ()=> useDispatch<AppDispatch>()
-export const useAppSelector: TypeUseSelectorHook<RootState> = useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
